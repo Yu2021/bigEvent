47cc984222affd692a4c3c85b17970c3d4c0d375
feiliangyu <feiliangyu@139.com>
2021-01-29 17:23:03 +0800

    完成了个人中心的基本资料模块

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,36 @@
+// 1. 获取用户的个人信息
+function getUserInfo () {
+  // 发送 ajax 请求
+  axios.get('/my/userinfo').then(res => {
+    // 校验请求失败
+    if (res.status !== 0) {
+      return layer.msg('获取用户信息失败!')
+    }
+
+    const { data } = res
+    // 渲染用户信息
+    // 1. 获取用户名
+    const name = data.nickname || data.username
+    // 2. 渲染昵称
+    $('.nickname').text(`欢迎 ${name}`).show()
+    // 3. 渲染头像
+    if (data.user_pic) {
+      // 注意: base64格式的编码字符串可以直接设置给 img 的 src 属性
+      $('.avatar').prop('src', data.user_pic).show()
+      $('.text-avatar').hide()
+    } else {
+      $('.text-avatar').text(name[0].toUpperCase()).show()
+      $('.avatar').hide()
+    }
+  })
+}
+
 $(function () {
+  // 调用获取个人信息函数
+  getUserInfo()
+
   // 从layui中提取模块
   const { layer } = layui
-
-  // 1. 获取用户的个人信息
-  function getUserInfo () {
-    // 发送 ajax 请求
-    axios.get('/my/userinfo').then(res => {
-      // 校验请求失败
-      if (res.status !== 0) {
-        return layer.msg('获取用户信息失败!')
-      }
-
-      const { data } = res
-      // 渲染用户信息
-      // 1. 获取用户名
-      const name = data.nickname || data.username
-      // 2. 渲染昵称
-      $('.nickname').text(`欢迎 ${name}`).show()
-      // 3. 渲染头像
-      if (data.user_pic) {
-        // 注意: base64格式的编码字符串可以直接设置给 img 的 src 属性
-        $('.avatar').prop('src', data.user_pic).show()
-        $('.text-avatar').hide()
-      } else {
-        $('.text-avatar').text(name[0].toUpperCase()).show()
-        $('.avatar').hide()
-      }
-    })
-  }
-
-  getUserInfo()
 
   // 2. 点击退出
   $('#logout').click(function () {
